afd8e7b64951ee6e182f7fa5f0bc9d46552456f6
SolDev <40839581+SolDev69@users.noreply.github.com>
2025-08-25 17:34:45 -0400

    Update JREUtils.java
    Test glesv3

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -226,7 +226,7 @@ public class JREUtils {
 			// TODO: merge with above block
 			if(LOCAL_RENDERER.equals("opengles3_egl")) {
 				envMap.put("LIBGL_ES", "3");
-				envMap.put("POJAVEXEC_EGL", "libEGL.so"); // Use system EGL
+				envMap.put("POJAVEXEC_EGL", "libGLESv3.so"); // Use system EGL
 			}
         }
         if(LauncherPreferences.PREF_BIG_CORE_AFFINITY) envMap.put("POJAV_BIG_CORE_AFFINITY", "1");
@@ -475,7 +475,7 @@ public class JREUtils {
                 renderLibrary = "libgl4es_114.so"; break;
             case "vulkan_zink": renderLibrary = "libOSMesa.so"; break;
             case "opengles3_ltw" : renderLibrary = "libltw.so"; break;
-			case "opengles3_egl" : renderLibrary = "libEGL.so"; break;
+			case "opengles3_egl" : renderLibrary = "libGLESv3.so"; break;
             default:
                 Log.w("RENDER_LIBRARY", "No renderer selected, defaulting to opengles2");
                 renderLibrary = "libgl4es_114.so";
